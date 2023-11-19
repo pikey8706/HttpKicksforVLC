9926b711f90da0638f27d7f7c4d11e0467df5a88
pikey8706 <pikey8706@gmail.com>
2023-11-19 23:00:55 +0900

    update for removal of androidx.lifecycle.Transformations

--- app/src/main/java/jp/pikey8706/httpkicksforvlc/ui/main/PageViewModel.kt
+++ app/src/main/java/jp/pikey8706/httpkicksforvlc/ui/main/PageViewModel.kt
@@ -1,12 +1,12 @@
 package jp.pikey8706.httpkicksforvlc.ui.main
 
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 
 class PageViewModel : ViewModel() {
     private val mChannel = MutableLiveData<String>()
-    val text = Transformations.map(mChannel) { channel -> "CH: $channel" }
+    val text = mChannel.map { channel -> "CH: $channel" }
 
     fun setChannel(channel: String) {
         mChannel.value = channel
